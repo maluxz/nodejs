2d17f2d0460b582d292285cb8041973c17cd4059
malux <malux97@hotmail.com>
2023-09-28 02:00:12 -0600

    Modulo OS

---
+++ conceptos/os.js
@@ -0,0 +1,29 @@
+const os = require('os');
+
+// INFORMACI[ON DEL SISTEMA]
+// console.log(os.arch());
+// console.log(os.platform());
+
+// NUMERO DE NUCLEOS
+// console.log(os.cpus().length);
+
+// CONSTANTES DEL SISTEMA
+// console.log(os.constants);
+
+// MEMORIA RAM LIBRE
+// const SIZE = 1024;
+// function kb(bytes) { return bytes / SIZE}
+// function mb(bytes) { return kb(bytes) / SIZE}
+// function gb(bytes) { return mb(bytes) / SIZE}
+// console.log(os.freemem() + ' bytes');
+// console.log(kb(os.freemem()) + ' kilo bytes');
+// console.log(mb(os.freemem()) + ' mega bytes');
+// console.log(gb(os.freemem()) + ' giga bytes');
+
+// DIRECTORIOS
+// console.log(os.homedir());
+// console.log(os.tmpdir());
+
+// NOMBRE DEL HOST E INTERFACES DE RED
+// console.log(os.hostname());
+// console.log(os.networkInterfaces());
